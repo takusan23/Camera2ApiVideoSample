99240dbd80e3b1bc21e77c34201648926d730aba
takusan23 <takumu1200@gmail.com>
2024-01-01 19:23:22 +0900

    デフォルト AV1

--- app/src/main/java/io/github/takusan23/camera2apivideosample/ui/component/RecorderSettingData.kt
+++ app/src/main/java/io/github/takusan23/camera2apivideosample/ui/component/RecorderSettingData.kt
@@ -3,7 +3,7 @@ package io.github.takusan23.camera2apivideosample.ui.component
 import io.github.takusan23.camera2apivideosample.recorder.CameraRecordInterface
 
 data class RecorderSettingData(
-    val codec: CameraRecordInterface.Codec = CameraRecordInterface.Codec.AVC,
+    val codec: CameraRecordInterface.Codec = CameraRecordInterface.Codec.AV1,
     val isForceSoftwareEncoder: Boolean = false,
     val width: Int = 1280,
     val height: Int = 720,
